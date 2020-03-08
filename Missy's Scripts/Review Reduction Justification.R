e2cd55fda3c6df20b729334592ef60ed3c7638a4
mlputur <53411848+mlputur@users.noreply.github.com>
2020-03-08 14:47:51 -0400

    Exploring if okay to delete reviews from people that wrote < 10 reviews.

--- Missy's Scripts/Review Reduction Justification.R
+++ Missy's Scripts/Review Reduction Justification.R
@@ -2,3 +2,14 @@ library(tidyverse)
 
 restaurants <- read_csv("Data/cananda_restaurant.csv")
 reviews <- read_csv("Data/canada_reviews.csv")
+
+
+ggplot(restaurants)+
+  geom_bar(aes(x=city,fill=factor(stars)),position="dodge")+
+  scale_fill_manual(values=c("#990000","#D32323","#F15C4F","#FF3300","#FF3366","#FF6666","#FFCCCC","#F8ADA8","#FCD6D3")) +
+  #scale_fill_gradient(low="yellow", high="red")+
+  #geom_bar(fill=c("#F15C4F", "#FCD6D3","#F8ADA8"
+  labs(title="Distribution of star ratings in Different City",
+       fill=guide_legend(title="Stars"))
+
+  
